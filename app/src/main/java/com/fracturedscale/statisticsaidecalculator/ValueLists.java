29e86d76c3fb50f044f4e214a3b2b68f040247d9
Spectrar <spectra1947@gmail.com>
2019-05-15 17:34:37 -0600

    determined key capture for next on list to be 66 and confirmed working

--- app/src/main/java/com/fracturedscale/statisticsaidecalculator/ValueLists.java
+++ app/src/main/java/com/fracturedscale/statisticsaidecalculator/ValueLists.java
@@ -2,10 +2,15 @@ package com.fracturedscale.statisticsaidecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class ValueLists extends AppCompatActivity {
+public class ValueLists extends AppCompatActivity implements View.OnKeyListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,12 @@ public class ValueLists extends AppCompatActivity {
 
         //adds action bar for back button in top left
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        LinearLayout l1 = findViewById(R.id.l1);
+        l1.setOnKeyListener(this);
+        EditText e5 = findViewById(R.id.editText6);
+        e5.setOnKeyListener(this);
+
     }
 
     /**
@@ -35,5 +46,15 @@ public class ValueLists extends AppCompatActivity {
      */
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
+    }
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+        String temp = Integer.toString(i);
+        Toast.makeText(ValueLists.this, temp, Toast.LENGTH_SHORT).show();
+        if (i==66){
+            //Toast.makeText(ValueLists.this, "Next", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
