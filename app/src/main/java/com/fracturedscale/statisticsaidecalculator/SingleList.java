6eb115cb400009895b241e8d6ed4d170793b5d3c
Spectrar <spectra1947@gmail.com>
2019-05-15 14:52:42 -0600

    Single list activity created with navigation to and from the activity established

---
+++ app/src/main/java/com/fracturedscale/statisticsaidecalculator/SingleList.java
@@ -0,0 +1,39 @@
+package com.fracturedscale.statisticsaidecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SingleList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_list);
+
+        //adds action bar for back button in top left
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //when back button is pressed, navigate to main activity
+        this.finish();
+        return true;
+    }
+
+    /**
+     *
+     * @param menu
+     * @return
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
