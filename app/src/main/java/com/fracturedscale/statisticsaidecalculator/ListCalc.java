3a70c17e8734dc668b4fc860de14fc5fe5930a8b
Spectrar <spectra1947@gmail.com>
2019-05-23 14:33:09 -0600

    Ajusted layout for data display

--- app/src/main/java/com/fracturedscale/statisticsaidecalculator/ListCalc.java
+++ app/src/main/java/com/fracturedscale/statisticsaidecalculator/ListCalc.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class ListCalc extends AppCompatActivity {
 
@@ -14,6 +18,18 @@ public class ListCalc extends AppCompatActivity {
 
         //adds action bar for back button in top left
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Spinner dataListSelector = findViewById(R.id.dataSpinner);
+        String[] temp1 = {"Values List","List 1","List 2","List 3","List 4"};
+        ArrayAdapter <String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, temp1);
+        dataListSelector.setAdapter(dataAdapter);
+
+        Spinner freqListSelector = findViewById(R.id.freqSpinner);
+        String[] temp2 = {"Frequency List","List 1","List 2","List 3","List 4"};
+        ArrayAdapter <String> freqAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, temp2);
+        freqListSelector.setAdapter(freqAdapter);
     }
 
     /**
