2e41317cad7559a9672f25af47a47549968997bb
Spectrar <spectra1947@gmail.com>
2019-06-12 16:54:28 -0600

    Started no list activity

---
+++ app/src/main/java/com/fracturedscale/statisticsaidecalculator/NoListCalcActivity.java
@@ -0,0 +1,41 @@
+package com.fracturedscale.statisticsaidecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NoListCalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_list_calc);
+
+        //adds action bar for back button in top left
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    /**
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //when back button is pressed, navigate to main activity
+        this.finish();
+        return true;
+    }
+
+    /**
+     *
+     * @param menu
+     * @return
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
