8ea7fb380b3341270efcd43bc9bdbee42a133d1b
Spectrar <spectra1947@gmail.com>
2019-07-16 16:57:02 -0600

    Normal CDF with popup is functioning

--- app/src/main/java/com/fracturedscale/statisticsaidecalculator/NoListCalcActivity.java
+++ app/src/main/java/com/fracturedscale/statisticsaidecalculator/NoListCalcActivity.java
@@ -1,11 +1,27 @@
 package com.fracturedscale.statisticsaidecalculator;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class NoListCalcActivity extends AppCompatActivity {
+public class NoListCalcActivity extends AppCompatActivity implements View.OnClickListener{
+
+    StatsHelper sh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +31,14 @@ public class NoListCalcActivity extends AppCompatActivity {
         //adds action bar for back button in top left
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        sh = new StatsHelper();
 
+        instantiateButtons();
+    }
+
+    private void instantiateButtons() {
+        Button normalCDF = findViewById(R.id.nCDF);
+        normalCDF.setOnClickListener(this);
     }
 
     /**
@@ -38,4 +61,59 @@ public class NoListCalcActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
+
+    @Override
+    public void onClick(final View view) {
+        final Dialog dialog = new Dialog(this);
+        Button calcButton = null;
+        Button cancelButton = null;
+        switch(view.getId()) {
+            case R.id.nCDF:
+                dialog.setContentView(R.layout.normalcdfpopup);
+                calcButton = (Button)dialog.findViewById(R.id.cdfcalculate);
+                cancelButton = (Button)dialog.findViewById(R.id.cdfcancel);
+                break;
+        }
+
+
+        dialog.setTitle("Save New Number");
+        dialog.setCancelable(true);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        //lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+
+        dialog.getWindow().setAttributes(lp);
+        dialog.show();
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                    TextView temp;
+                    switch(view.getId()){
+                        case R.id.nCDF:
+                            temp = findViewById(R.id.nCDFResults);
+                            //temp.setText(String.valueOf(sh.normalCDF(0.0,3.0,0.0,1.0))+".499");
+                            temp.setText(String.valueOf(sh.normalCDF(Double.valueOf(((EditText) dialog.findViewById(R.id.cdflower)).getText().toString())
+                                    ,Double.valueOf(((EditText) dialog.findViewById(R.id.cdfupper)).getText().toString())
+                                    ,Double.valueOf(((EditText) dialog.findViewById(R.id.cdfmean)).getText().toString())
+                                    ,Double.valueOf(((EditText) dialog.findViewById(R.id.cdfsd)).getText().toString()))));
+                            dialog.dismiss();
+                            break;
+                    }
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+
+
 }
--- app/src/main/java/com/fracturedscale/statisticsaidecalculator/StatsHelper.java
+++ app/src/main/java/com/fracturedscale/statisticsaidecalculator/StatsHelper.java
@@ -2,9 +2,17 @@ package com.fracturedscale.statisticsaidecalculator;
 
 import org.apache.commons.math3.stat.inference.BinomialTest;
 import org.apache.commons.math3.stat.interval.BinomialConfidenceInterval;
+import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class StatsHelper {
 
     BinomialConfidenceInterval binomPDF;
+
+    private NormalDistribution nD;
+
+    public double normalCDF(double lower, double upper, double mean, double sd){
+        nD = new NormalDistribution(mean,sd);
+        return nD.probability(lower,upper);
+    }
 
 }
